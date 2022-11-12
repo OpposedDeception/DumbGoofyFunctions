f64690f4b9eda6e74c0201a582a46793503ab5b6
KotenAngered <113610915+KotenAngered@users.noreply.github.com>
2022-11-13 00:06:03 +0100

    Update dumbideas.py

--- dumbideas.py
+++ dumbideas.py
@@ -1,4 +1,5 @@
 from random import randint
+from itertools import counter
 
 # Basically, it prints 4 lists with random value, and every list should have technically bigger number
 # But at the end it gives ValueError for some reason. I'll resolve it someday.
@@ -17,17 +18,30 @@ def ranlist():
         print("You created lists")
         input("Press ENTER to exit")
         exit()
+        
+# It will count values. I'll finish this ASAP
+def data_couner(x, y):
+    data_1 = [x]
+    data_2 = [y]
+    return data_1 + data_2
 
 print("1. Output strange result")
-choice = ('1')
+print("2. Count data")
+choice = ('1', '2')
 user_choice = int(input("Write your choice: "))
 
 try:
     if user_choice == 1:
       ranlist()
+      if user_choice == 2:
+            NUM1 = float(input("Type first number: "))
+            NUM2 = float(input("Type second number: "))
+            result = data_counter(NUM1, NUM2)
+            print(result)
 except ValueError:
     print("You had to write a number")
     input("Press ENTER to exit")
     exit()
 
 # Function 1 - 11.11.2022
+# Function 2 - 13.11.2022
